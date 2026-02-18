168068dd77b4bec16b0c2b2299e2ef1991bdb625
LeandroVC <leandrovicttorio78@gmail.com>
2026-02-18 10:17:50 -0300

    add: send screen

--- src/routes/send.tsx
+++ src/routes/send.tsx
@@ -9,18 +9,41 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 import { Field } from '@/components/ui/field'
 import { UserResponse } from '@/types/users'
 import { Users } from '@/components/notifications/SendUsersList'
+import { sendNotification } from '@/services/events.service'
+import { send } from '@/types/send'
+import { DialogClose } from '@/components/ui/dialog'
 
 export const Route = createFileRoute('/send')({
-  component: CardDemo,
+  component: SendPage,
 });
 
-function CardDemo() {
+export function SendPage() {
+  const timestamp = new Date().toISOString()
   const [users, setUsers] = useState<UserResponse[]>([]);
+  const [notifyData, setNotifyData] = useState<send>({
+    userId: '',
+    type: 'info',
+    title: '',
+    message: '',
+    data: {
+      timestamp,
+    },
+  });
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
-  const [sendNotification, setSendNotification] = useState({
-    id: '',
-  })
+
+  function handleSend(e: React.FormEvent) {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = sendNotification(notifyData);
+      console.log(response);
+    } catch (e) {
+      return e;
+    }
+
+  }
 
   useEffect(() => {
     let cancelled = false;
@@ -28,7 +51,7 @@ function CardDemo() {
     getAllUsers()
       .then(data => {
         if (!cancelled) {
-          setUsers(data);
+          setUsers(data.data);
         }
       })
       .catch(err => {
@@ -47,9 +70,9 @@ function CardDemo() {
   }, []);
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-slate-50 to-slate-100 dark:from-slate-950 dark:to-slate-900 p-4">
-      <div className="w-full max-w-md">
-        <Card className="w-full max-w-sm">
+    <div className="flex min-h-full  items-center justify-center bg-gradient-to-br from-slate-50 to-slate-100 dark:from-slate-950 dark:to-slate-900 p-4">
+      <div className="w-full max-w-7xl">
+        <Card className="w-full ">
           <CardHeader>
             <CardTitle>Enviar notificação</CardTitle>
             <CardDescription>
@@ -57,26 +80,34 @@ function CardDemo() {
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <form>
+            <form onSubmit={handleSend}>
               <div className="flex flex-col gap-6">
                 <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
+                  <Label htmlFor="email">Titulo</Label>
                   <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
+                    id="titulo"
+                    type="titulo"
+                    value={notifyData.title}
+                    onChange={(e) => {
+                      setNotifyData({ ...notifyData, title: e.target.value })
+                    }}
+                    placeholder="Titulo"
                     required
                   />
                 </div>
                 <div className="grid gap-2">
                   <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
+                    <Label htmlFor="message">Mensagem</Label>
                   </div>
-                  <Input id="password" type="password" required />
+                  <Input id="message" type="message" value={notifyData.message} onChange={(e) => {
+                    setNotifyData({ ...notifyData, message: e.target.value })
+                  }} required />
                 </div>
                 <div>
                   <Field>
-                    <Select>
+                    <Select value={notifyData.userId} onValueChange={(value) =>
+                      setNotifyData((prev) => ({ ...prev, userId: value }))
+                    }>
                       <SelectTrigger className="w-full max-w-48">
                         <SelectValue placeholder="Selecione Usuario" />
                       </SelectTrigger>
@@ -87,15 +118,14 @@ function CardDemo() {
                   </Field>
                 </div>
               </div>
+              <DialogClose asChild>
+                <Button type="submit" className="w-full mt-4">
+                  Enviar
+                </Button>
+              </DialogClose>
             </form>
           </CardContent>
           <CardFooter className="flex-col gap-2">
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
           </CardFooter>
         </Card>
       </div>
